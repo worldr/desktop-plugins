54c3ecc6b8fa67161c0238cbd46dbc0fab727c0f
Nox <feel@noregret.org>
2020-09-29 15:37:10 +0300

    wip

--- app-badge/platforms/darwin.h
+++ app-badge/platforms/darwin.h
@@ -1,17 +1,19 @@
 #import <Cocoa/Cocoa.h>
 int
-PlatformSetWindowTitle(NSString value) {
+PlatformSetWindowTitle(NSString* value) {
 	NSWindow *window = [[[NSApplication sharedApplication] windows] objectAtIndex:0];
 	window.title = value;
   return 0;
 }
-NSString
+NSString*
 PlatformGetWindowTitle() {
 	NSWindow *window = [[[NSApplication sharedApplication] windows] objectAtIndex:0];
   return window.title;
 }
 int
 PlatformSetBadge(int value) {
-	[NSApplication sharedApplication].applicationIconBadgeNumber = value;
+	NSString* str = [NSString stringWithFormat:@"%i", value];
+	NSDockTile* tile = [[NSApplication sharedApplication] dockTile];
+	[tile setBadgeLabel:str];	
   return 0;
 }
